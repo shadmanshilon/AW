aa5093f6584cbb1a859462785b5b484b1a5b13d1
shadmanshilon <shadman.shilon@allgentech.io>
2025-11-26 18:40:55 +0600

    push for task 1 and 2
    push for task 1 and 2

---
+++ src/main/java/com/allied/framework/pages/AssignLeavePage.java
@@ -0,0 +1,41 @@
+package com.allied.framework.pages;
+
+import org.openqa.selenium.By;
+
+public class AssignLeavePage {
+
+    // Navigation
+    public static final By LEAVE_MENU = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-navigation > aside > nav > div.oxd-sidepanel-body > ul > li:nth-child(3) > a > span");
+    public static final By ASSIGN_LEAVE = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-navigation > header > div.oxd-topbar-body > nav > ul > li:nth-child(7)");
+    // Thread.sleep(5000);
+    // Form fields
+    public static final By EMPLOYEE_NAME_INPUT = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div > div > div:nth-child(2) > div > div > input");
+    
+    public static final By EMPOLYEE_CLICK = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(1) > div > div > div > div:nth-child(2) > div > div > div.oxd-autocomplete-text-input--after");
+    
+
+    public static final By LEAVE_TYPE_SELECT = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(2) > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.oxd-select-text-input");
+
+    
+    public static final By LEAVE_TYPE_OPTION = By.xpath("//div[@role='option']"); // generic option list
+
+
+    // public static final By ICON_TO = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(2) > div > div > i");
+
+    public static final By FROM_DATE_INPUT = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(2) > div > div > input");
+    
+    
+    public static final By TO_DATE_INPUT   = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(3) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > input");
+
+    public static final By CLICK = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(4) > div")
+
+    public static final By DURATION_SELECT  = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(4) > div > div:nth-child(1) > div > div:nth-child(2) > div > div");
+    public static final By DURATION_OPTION  = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(4) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div.oxd-select-text-input"); // generic
+
+    public static final By COMMENT_TEXTAREA  = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div:nth-child(5) > div > div > div > div:nth-child(2) > textarea");
+
+    public static final By ASSIGN_BUTTON    = By.cssSelector("#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.oxd-form-actions > button");
+
+    // Confirmation or resulting message
+    public static final By ASSIGN_SUCCESS_MSG = By.xpath("//div[contains(text(),'Successfully Assigned')]");
+}
---
+++ src/main/java/com/allied/framework/pages/LeavePage.java
@@ -0,0 +1,19 @@
+package com.allied.framework.pages;
+
+import org.openqa.selenium.By;
+
+public class LeavePage {
+
+    // Menu navigation
+    public static final By LEAVE_MENU = By.xpath("//span[text()='Leave']");
+    public static final By LEAVE_LIST_SUBMENU = By.xpath("//a[contains(@href,'viewLeaveList')]");
+
+    // Filters
+    public static final By EMPLOYEE_NAME_INPUT = By.xpath("//input[@placeholder='Type for hints...']");
+    public static final By FROM_DATE_INPUT = By.xpath("//label[text()='From Date']/following::input[1]");
+    public static final By TO_DATE_INPUT = By.xpath("//label[text()='To Date']/following::input[1]");
+    public static final By SEARCH_BUTTON = By.xpath("//button[normalize-space()='Search']");
+
+    // Result table
+    public static final By RESULT_TABLE = By.xpath("//div[@class='oxd-table-body']");
+}
